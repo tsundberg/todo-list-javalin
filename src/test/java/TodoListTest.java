6c5b7a4fb7f4d618f74b6a6d15b801172ea505c5
Thomas Sundberg <thomas@thinkcode.se>
2024-08-02 11:19:36 +0200

    Setup project

---
+++ src/test/java/TodoListTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TodoListTest {
+
+    private final TodoList todoList = new TodoList();
+
+    @Test
+    void should_create_the_todo_app() {
+        assertThat(todoList).isNotNull();
+    }
+}
